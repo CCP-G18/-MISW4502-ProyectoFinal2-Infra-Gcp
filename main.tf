04a2fdee670ba1e6c1e5c0cca26f4509eb3269bb
Jhon Andrés Parra R. <ja.parrar12@uniandes.edu.co>
2025-03-25 06:15:09 -0500

    Setup CD in repository

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "google" {
   project = "proyecto-final-452204"
-  region = "us-central1"
+  region  = "us-central1"
 }
 
 resource "google_artifact_registry_repository" "uniandes_misw_proyecto_final" {
   repository_id = "uniandes-misw-proyecto-final-2"
-  format = "DOCKER"
-  location = "us-central1"
+  format        = "DOCKER"
+  location      = "us-central1"
 
   docker_config {
     immutable_tags = false
